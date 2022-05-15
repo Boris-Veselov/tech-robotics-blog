2bb704e510551f9eb7d4b5d1351ef694292d867d
Boris Veselov <borisveselov@yahoo.com>
2022-05-15 13:15:00 -0400

    updated controllers

--- server.js
+++ server.js
@@ -29,10 +29,10 @@ const sequelize = require('./config/connection');
 // app.set('view engine', 'handlebars');
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(require('./controllers/'));
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
